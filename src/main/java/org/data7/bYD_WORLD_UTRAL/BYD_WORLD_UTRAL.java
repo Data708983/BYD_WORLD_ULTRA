ebdc53216ee5fc9043f9f46620a8f64cdf02d578
Data708983 <2773277350@qq.com>
2025-07-02 15:35:24 +0800

    🍼无关紧要的改进

--- src/main/java/org/data7/bYD_WORLD_UTRAL/BYD_WORLD_UTRAL.java
+++ src/main/java/org/data7/bYD_WORLD_UTRAL/BYD_WORLD_UTRAL.java
@@ -1,5 +1,7 @@
 package org.data7.bYD_WORLD_UTRAL;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BYD_WORLD_UTRAL extends JavaPlugin {
@@ -7,6 +9,13 @@ public final class BYD_WORLD_UTRAL extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getServer().getLogger().info("\n ____  _  _  ____        __  __  ____  ____    __    __   \n" +
+                "(  _ \\( \\/ )(  _ \\      (  )(  )(_  _)(  _ \\  /__\\  (  )  \n" +
+                " ) _ < \\  /  )(_) ) ___  )(__)(   )(   )   / /(__)\\  )(__ \n" +
+                "(____/ (__) (____/ (___)(______) (__) (_)\\_)(__)(__)(____)");
+        getServer().getLogger().info("BYD_WORLD_UTRAL is enabled!");
+        String version = getDescription().getVersion();
+        getServer().getLogger().info("Plugin version:" + "\033[1;34m"+ version +"\033[0m");
 
     }
 
---
+++ src/main/java/org/data7/bYD_WORLD_UTRAL/PlayerJoinEvent.java
@@ -0,0 +1,13 @@
+package org.data7.bYD_WORLD_UTRAL;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class PlayerJoinEvent {
+    public class PlayerListener implements Listener{
+        @EventHandler
+        public void playerJoin(){
+
+        }
+    }
+}
