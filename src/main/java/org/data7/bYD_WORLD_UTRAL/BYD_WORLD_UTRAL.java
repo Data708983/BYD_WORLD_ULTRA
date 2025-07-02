d6ae38ee1c6fa5e11689de9ee4785f83e4cf3aab
Data708983 <2773277350@qq.com>
2025-07-02 15:03:58 +0800

    initial commit

---
+++ src/main/java/org/data7/bYD_WORLD_UTRAL/BYD_WORLD_UTRAL.java
@@ -0,0 +1,17 @@
+package org.data7.bYD_WORLD_UTRAL;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BYD_WORLD_UTRAL extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
