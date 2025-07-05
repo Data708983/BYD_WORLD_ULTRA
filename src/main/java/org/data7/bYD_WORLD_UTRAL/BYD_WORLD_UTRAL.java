aa53df6e8a572dd057e218c04931a15120ffe811
Data708983 <2773277350@qq.com>
2025-07-06 00:58:44 +0800

    ☃️/tpa完成

--- src/main/java/org/data7/bYD_WORLD_UTRAL/BYD_WORLD_UTRAL.java
+++ src/main/java/org/data7/bYD_WORLD_UTRAL/BYD_WORLD_UTRAL.java
@@ -1,22 +1,19 @@
 package org.data7.bYD_WORLD_UTRAL;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Bukkit;
-import org.bukkit.Color;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.data7.bYD_WORLD_UTRAL.PlayerJoin;
-import org.data7.bYD_WORLD_UTRAL.Tpa;
-import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.UUID;
+
+import static org.data7.bYD_WORLD_UTRAL.Tpa.*;
 
 public final class BYD_WORLD_UTRAL extends JavaPlugin {
 
@@ -29,7 +26,7 @@ public final class BYD_WORLD_UTRAL extends JavaPlugin {
                 "(____/ (__) (____/ (___)(______) (__) (_)\\_)(__)(__)(____)");
         getServer().getLogger().info("BYD_WORLD_UTRAL is enabled!");
         String version = getDescription().getVersion();
-        getServer().getLogger().info("Plugin version:" + "\033[1;34m"+ version +"\033[0m");
+        getServer().getLogger().info("Plugin version:" + "\033[1;34m" + version + "\033[0m");
 
         //配置文件
         File config = new File(getDataFolder(), "config.yml");
@@ -65,20 +62,19 @@ public final class BYD_WORLD_UTRAL extends JavaPlugin {
     }
 
     /**
-     *
-     * @param sender Source of the command
+     * @param sender  Source of the command
      * @param command Command which was executed
-     * @param label Alias of the command which was used
-     * @param args Passed command arguments
+     * @param label   Alias of the command which was used
+     * @param args    Passed command arguments
      * @return
      */
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         /**
          * 紫砂
          */
-        if (label.equalsIgnoreCase("suicide")){
-            if ( sender instanceof Player){
+        if (label.equalsIgnoreCase("suicide")) {
+            if (sender instanceof Player) {
                 Player player = (Player) sender;
                 Component suicideMessage = Component.text("💀")
                         .append(Component.text(player.getName())
@@ -88,14 +84,207 @@ public final class BYD_WORLD_UTRAL extends JavaPlugin {
                 // 获取所有在线玩家
                 for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
                     // 为每个在线玩家播放音效
-                    onlinePlayer.playSound(player.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 5f, 5f);}
+                    onlinePlayer.playSound(player.getLocation(), Sound.ENTITY_GENERIC_EXPLODE, 5f, 5f);
+                }
                 player.addScoreboardTag("suicide");
                 player.setHealth(0);
-
                 return true;
             }
         }
+        /** TPA */
+        if (label.equalsIgnoreCase("tpa")) {
 
+            Tpa.TpaConfig tpaConfig = loadTpa();
+
+            if (tpaConfig.isEnableTpa()) {
+                if (sender instanceof Player) {
+                    Player player = (Player) sender;
+
+                    // 检查参数数量
+                    if (args.length != 2) {
+                        player.sendMessage(Component.text("❎命令用法错误！正确格式: /tpa to <玩家名> 或 /tpa come <玩家名>")
+                                .color(TextColor.color(255, 0, 0)));
+                        return true;
+                    }
+
+                    String subCommand = args[0];
+                    String targetName = args[1];
+                    Player targetPlayer = Bukkit.getPlayerExact(targetName); // 精确查找玩家
+
+                    // 检查目标玩家是否在线
+                    if (targetPlayer == null || !targetPlayer.isOnline()) {
+                        player.sendMessage(Component.text("❎找不到玩家: " + targetName)
+                                .color(TextColor.color(255, 0, 0)));
+                        return true;
+                    }
+
+                    UUID playercome; // 请求发起者
+                    UUID playerto;   // 请求目标
+
+                    if (subCommand.equalsIgnoreCase("to")) {
+                        playercome = player.getUniqueId();
+                        playerto = targetPlayer.getUniqueId();
+
+                        if (isOnCoolDown(playercome.toString(), tpaConfig.getCooldown(), tpaConfig.getReply()) == -1) {
+
+                            if(tpaConfig.isConfirm()){
+                                // 发送提示信息
+                                player.sendMessage(Component.text("已向 " + targetName + " 发送传送请求（你将传送到对方）"));
+                                Component requestMessage = Component.text()
+                                        .append(Component.text(player.getName(), TextColor.color(255, 255, 0))
+                                                .append(Component.text(" 请求传送到你这里!", TextColor.color(255, 255, 225))))
+                                        .build();
+                                targetPlayer.sendMessage(requestMessage);
+
+                                Component responseButtons = Component.text()
+                                        .append(Component.text("[接受] ", TextColor.color(0, 255, 0))
+                                                .clickEvent(ClickEvent.callback(clicker -> {
+                                                    // 接受请求后的消息
+                                                    Component acceptTargetMsg = Component.text()
+                                                            .append(Component.text(player.getName(), TextColor.color(255, 255, 0))
+                                                                    .append(Component.text(" 传送过来了!", TextColor.color(255, 255, 225))))
+                                                            .build();
+
+                                                    Component acceptPlayerMsg = Component.text()
+                                                            .append(Component.text(targetPlayer.getName(), TextColor.color(255, 255, 0))
+                                                                    .append(Component.text(" 同意了你的请求!", TextColor.color(255, 255, 225))))
+                                                            .build();
+                                                    tpa(playercome.toString(),playerto.toString());
+
+                                                    targetPlayer.sendMessage(acceptTargetMsg);
+                                                    player.sendMessage(acceptPlayerMsg);
+                                                }))
+                                                .append(Component.text(" "))
+                                                .append(Component.text("[拒绝]", TextColor.color(255, 0, 0))
+                                                        .clickEvent(ClickEvent.callback(clicker -> {
+                                                            // 拒绝请求后的消息
+                                                            Component rejectTargetMsg = Component.text()
+                                                                    .append(Component.text("你拒绝了 ", TextColor.color(255, 255, 225)))
+                                                                    .append(Component.text(player.getName(), TextColor.color(255, 255, 0))
+                                                                            .append(Component.text(" 的传送请求!", TextColor.color(255, 255, 225))))
+                                                                    .build();
+
+                                                            Component rejectPlayerMsg = Component.text()
+                                                                    .append(Component.text(targetPlayer.getName(), TextColor.color(255, 255, 0))
+                                                                            .append(Component.text(" 拒绝了你的请求!", TextColor.color(255, 255, 225))))
+                                                                    .build();
+
+                                                            targetPlayer.sendMessage(rejectTargetMsg);
+                                                            player.sendMessage(rejectPlayerMsg);
+                                                        }))))
+                                        .build();
+
+                                targetPlayer.sendMessage(responseButtons);
+                            }
+                            else {
+                                Component acceptTargetMsg = Component.text()
+                                        .append(Component.text(player.getName(), TextColor.color(255, 255, 0))
+                                                .append(Component.text(" 传送过来了!", TextColor.color(255, 255, 225))))
+                                        .build();
+
+                                Component acceptPlayerMsg = Component.text()
+                                        .append(Component.text("你传送到了 ", TextColor.color(255, 255, 225)))
+                                                .append(Component.text(targetPlayer.getName(), TextColor.color(255, 255, 0)))
+                                        .build();
+                                tpa(playercome.toString(),playerto.toString());
+
+                                targetPlayer.sendMessage(acceptTargetMsg);
+                                player.sendMessage(acceptPlayerMsg);
+                            }
+
+                        }
+
+                    }
+                    else if (subCommand.equalsIgnoreCase("come")) {
+                        playerto = player.getUniqueId();
+                        playercome = targetPlayer.getUniqueId();
+                        if(tpaConfig.isConfirm()){
+                            // 发送提示信息
+                            player.sendMessage(Component.text("已向 " + targetName + " 发送传送请求（请求对方传送到你）"));
+                            // 构建请求消息
+                            Component requestMessage = Component.text()
+                                    .append(Component.text(player.getName(), TextColor.color(255, 255, 0)))
+                                    .append(Component.text(" 请求你传送到TA那里!", TextColor.color(255, 255, 225)))
+                                    .build();
+                            targetPlayer.sendMessage(requestMessage);
+                            // 构建交互按钮
+                            Component responseButtons = Component.text()
+                                    .append(Component.text("[接受] ", TextColor.color(0, 255, 0))
+                                            .clickEvent(ClickEvent.callback(clicker -> {
+                                                // 接受请求后的消息
+                                                if (isOnCoolDown(playercome.toString(), tpaConfig.getCooldown(), tpaConfig.getReply()) == -1){
+                                                    Component acceptTargetMsg = Component.text()
+                                                            .append(Component.text("你已传送到 ", TextColor.color(255, 255, 225)))
+                                                            .append(Component.text(player.getName(), TextColor.color(255, 255, 0)))
+                                                            .append(Component.text(" 的位置!", TextColor.color(255, 255, 225)))
+                                                            .build();
+                                                    Component acceptPlayerMsg = Component.text()
+                                                            .append(Component.text(targetPlayer.getName(), TextColor.color(255, 255, 0)))
+                                                            .append(Component.text(" 已传送到你这里!", TextColor.color(255, 255, 225)))
+                                                            .build();
+                                                    // 执行传送（对方传送到自己）
+                                                    tpa(playercome.toString(), playerto.toString());
+                                                    targetPlayer.sendMessage(acceptTargetMsg);
+                                                    player.sendMessage(acceptPlayerMsg);
+                                                }
+                                            }))
+                                            .append(Component.text(" "))
+                                            .append(Component.text("[拒绝]", TextColor.color(255, 0, 0))
+                                                    .clickEvent(ClickEvent.callback(clicker -> {
+                                                        // 拒绝请求后的消息
+                                                        Component rejectTargetMsg = Component.text()
+                                                                .append(Component.text("你拒绝了 ", TextColor.color(255, 255, 225)))
+                                                                .append(Component.text(player.getName(), TextColor.color(255, 255, 0)))
+                                                                .append(Component.text(" 的传送请求!", TextColor.color(255, 255, 225)))
+                                                                .build();
+                                                        Component rejectPlayerMsg = Component.text()
+                                                                .append(Component.text(targetPlayer.getName(), TextColor.color(255, 255, 0)))
+                                                                .append(Component.text(" 拒绝了你的传送请求!", TextColor.color(255, 255, 225)))
+                                                                .build();
+                                                        targetPlayer.sendMessage(rejectTargetMsg);
+                                                        player.sendMessage(rejectPlayerMsg);
+                                                    }))))
+                                    .build();
+                            targetPlayer.sendMessage(responseButtons);
+                        }
+                        else {
+                            long timerest = isOnCoolDown(playercome.toString(), tpaConfig.getCooldown(), tpaConfig.getReply());
+                            if (timerest == -1){
+                                Component acceptTargetMsg = Component.text()
+                                        .append(Component.text("你已传送到 ", TextColor.color(255, 255, 225)))
+                                        .append(Component.text(player.getName(), TextColor.color(255, 255, 0)))
+                                        .append(Component.text(" 的位置!", TextColor.color(255, 255, 225)))
+                                        .build();
+                                Component acceptPlayerMsg = Component.text()
+                                        .append(Component.text(targetPlayer.getName(), TextColor.color(255, 255, 0)))
+                                        .append(Component.text(" 已传送到你这里!", TextColor.color(255, 255, 225)))
+                                        .build();
+                                // 执行传送（对方传送到自己）
+                                tpa(playercome.toString(), playerto.toString());
+                                targetPlayer.sendMessage(acceptTargetMsg);
+                                player.sendMessage(acceptPlayerMsg);
+                            }
+                            else {
+                                Component acceptTargetMsg1 = Component.text()
+                                        .append(Component.text(player.getName(), TextColor.color(255, 255, 0)))
+                                        .append(Component.text("请求你的援助! 但你还在冷却中~", TextColor.color(255, 255, 225)))
+                                        .build();
+                                player.sendMessage(Component.text("❎TA仍在冷却中，剩余时间: ",TextColor.color(255,0,0)).append(Component.text(timerest + "s")));
+                                targetPlayer.sendMessage(acceptTargetMsg1);
+                            }
+
+                        }
+                    }
+                    else {
+                        player.sendMessage(Component.text("❎命令用法错误！正确格式: /tpa to <玩家名> 或 /tpa come <玩家名>")
+                                .color(TextColor.color(255, 0, 0)));
+                        return true;
+                    }
+                    return true;
+                }
+            } else return false;
+
+        }
         return false;
     }
 
--- src/main/java/org/data7/bYD_WORLD_UTRAL/Tpa.java
+++ src/main/java/org/data7/bYD_WORLD_UTRAL/Tpa.java
@@ -1,6 +1,18 @@
 package org.data7.bYD_WORLD_UTRAL;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import java.io.File;
 import java.sql.*;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 public class Tpa {
     public void connect() {
@@ -26,20 +38,21 @@ public class Tpa {
              Statement stmt = conn.createStatement()) {
             // 执行SQL语句
             stmt.execute(sql);
-            System.out.println("表创建成功（如果不存在）");
+//            System.out.println("表创建成功（如果不存在）");
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+//            System.out.println(e.getMessage());
         }
 
+        loadTpa();
 //        // 插入数据的SQL语句
 //        String insertSql = "INSERT INTO TPA (uuidfrom, uuidto, timestemp, available) VALUES (?, ?, ?, ?)";
 //
 //        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:plugins/BYD_WORLD_UTRAL/database.db");
 //             PreparedStatement pstmt = conn.prepareStatement(insertSql)) {
 //            // 设置插入的数据
-//            pstmt.setString(1, "asdasdasdasd");
-//            pstmt.setString(2, "zxczxczxczcx");
-//            pstmt.setInt(3, 114514);
+//            pstmt.setString(1, "e4037d10-e436-35e4-9c45-594a12ff44d0");
+//            pstmt.setString(2, "e4037d10-e436-35e4-9c45-594a12ff44d0");
+//            pstmt.setLong(3, 114514);
 //            pstmt.setInt(4, 1);
 //
 //            /*用于测试 */
@@ -47,23 +60,161 @@ public class Tpa {
 //            pstmt.executeUpdate();
 //            System.out.println("数据插入成功");
 //
-//            // 读取数据的SQL语句
-//            String readSql = "SELECT * FROM TPA";
-//
-//            try (Statement stmt = conn.createStatement();
-//                 ResultSet rs = stmt.executeQuery(readSql)) {
-//                // 输出读取的数据
-//                while (rs.next()) {
-//                    System.out.println("id: " + rs.getInt("id"));
-//                    System.out.println("uuidfrom: " + rs.getString("uuidfrom"));
-//                    System.out.println("uuidto: " + rs.getString("uuidto"));
-//                    System.out.println("timestemp: " + rs.getInt("timestemp"));
-//                    System.out.println("available: " + rs.getInt("available"));
-//                }
-//            }
-//
 //        } catch (SQLException e) {
 //            System.out.println(e.getMessage());
 //        }
+    }
+    /** 加载 TPA 传送模块: tpa.yml */
+    public static class TpaConfig {
+        private boolean enableTpa;
+        private boolean debug;
+        private long cooldown;
+        private int reply;
+        private boolean confirm;
+        private String type;
+        private int record;
+
+        // 构造函数
+        public TpaConfig(boolean enableTpa, boolean debug, long cooldown, int reply, boolean confirm, String type, int record) {
+            this.enableTpa = enableTpa;
+            this.debug = debug;
+            this.cooldown = cooldown;
+            this.reply = reply;
+            this.confirm = confirm;
+            this.type = type;
+            this.record = record;
+        }
+
+        // Getter 方法
+        public boolean isEnableTpa() { return enableTpa; }
+        public boolean isDebug() { return debug; }
+        public long getCooldown() { return cooldown; }
+        public int getReply() { return reply; }
+        public boolean isConfirm() { return confirm; }
+        public String getType() { return type; }
+        public int getRecord() { return record; }
+    }
+
+    public static TpaConfig loadTpa() {
+        BYD_WORLD_UTRAL plugin = BYD_WORLD_UTRAL.getPlugin(BYD_WORLD_UTRAL.class);
+        File tpaFile = new File(plugin.getDataFolder(), "tpa.yml");
+        YamlConfiguration tpaConfig = YamlConfiguration.loadConfiguration(tpaFile);
+
+        // 如果文件不存在，创建默认配置
+        if (!tpaFile.exists()) {
+            // 保存默认配置（注意：这里应该保存 tpa.yml 而不是 config.yml）
+            plugin.saveResource("tpa.yml", false);
+            tpaConfig = YamlConfiguration.loadConfiguration(tpaFile);
+        }
+
+        // 读取所有配置值
+        boolean enableTpa = tpaConfig.getBoolean("enable", true);
+        boolean debug = tpaConfig.getBoolean("debug", false);
+        long cooldown = tpaConfig.getLong("cooldown", 60); // 默认30秒
+        int reply = tpaConfig.getInt("reply", 60); // 默认60秒
+        boolean confirm = tpaConfig.getBoolean("confirm", true);
+        String type = tpaConfig.getString("type", "teleport");
+        int record = tpaConfig.getInt("record", 20); // 默认记录20条
+
+        // 在debug模式下打印数据库记录
+        if(debug) {
+            String readSql = "SELECT * FROM TPA";
+            try (Connection conn = DriverManager.getConnection("jdbc:sqlite:plugins/BYD_WORLD_UTRAL/database.db");
+                 Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery(readSql)) {
+
+                while (rs.next()) {
+                    Bukkit.getServer().getLogger().info("id: " + rs.getInt("id"));
+                    Bukkit.getServer().getLogger().info("uuidfrom: " + rs.getString("uuidfrom"));
+                    Bukkit.getServer().getLogger().info("uuidto: " + rs.getString("uuidto"));
+                    Bukkit.getServer().getLogger().info("timestemp: " + rs.getLong("timestemp"));
+                    Bukkit.getServer().getLogger().info("available: " + rs.getInt("available"));
+                }
+            } catch (SQLException e) {
+                Bukkit.getServer().getLogger().log(Level.SEVERE, "数据库查询错误", e);
+            }
+        }
+
+        // 返回包含所有配置值的对象
+        return new TpaConfig(enableTpa, debug, cooldown, reply, confirm, type, record);
+    }
+
+    public static long isOnCoolDown(String playeruuid, long cooldown, int reply) {
+        // 获取现在的时间戳
+        long now_time = System.currentTimeMillis();
+        // 修改SQL以获取最近的一条记录（按id降序）
+        String readSql = "SELECT * FROM TPA WHERE uuidfrom = ? ORDER BY id DESC LIMIT 1";
+
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:plugins/BYD_WORLD_UTRAL/database.db");
+             PreparedStatement pstmt = conn.prepareStatement(readSql)) {
+
+            pstmt.setString(1, playeruuid);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                long recordTimestamp = rs.getLong("timestemp");
+                long timeDifference = now_time - recordTimestamp;
+                if(loadTpa().isDebug()){
+                    Bukkit.getServer().getLogger().info("查询到最近记录:");
+                    Bukkit.getServer().getLogger().info("id: " + rs.getInt("id"));
+                    Bukkit.getServer().getLogger().info("uuidfrom: " + rs.getString("uuidfrom"));
+                    Bukkit.getServer().getLogger().info("uuidto: " + rs.getString("uuidto"));
+                    Bukkit.getServer().getLogger().info("timestemp: " + recordTimestamp);
+                    Bukkit.getServer().getLogger().info("available: " + rs.getInt("available"));
+                    Bukkit.getServer().getLogger().info("与当前时间差: " + timeDifference + "ms");
+                }
+                // 检查时间差是否小于冷却时间
+                if (timeDifference < cooldown*1000) {
+                    long remainingTime = (cooldown * 1000 - timeDifference)/1000;
+                    Player fromPlayer = Bukkit.getPlayer(UUID.fromString(playeruuid));
+                    fromPlayer.sendMessage(Component.text("❎仍在冷却中，剩余时间: ",TextColor.color(255,0,0)).append(Component.text(remainingTime + "s")));
+                    return remainingTime;
+                }
+            }
+        } catch (SQLException e) {
+            Bukkit.getServer().getLogger().log(Level.SEVERE, "数据库查询错误", e);
+        }
+        if(loadTpa().isDebug()) Bukkit.getServer().getLogger().info("无冷却限制");
+        return -1;
+    }
+    public static boolean tpa(String playerfrom,String playerto){
+        // 获取现在的时间戳
+        long now_time = System.currentTimeMillis();
+        // 获取玩家对象
+        Player fromPlayer = Bukkit.getPlayer(UUID.fromString(playerfrom));
+        Player toPlayer = Bukkit.getPlayer(UUID.fromString(playerto));
+        // 获取配置类型
+        String type = loadTpa().getType();
+
+        // 根据类型执行不同的传送方式
+        if (type.equalsIgnoreCase("absolute")) {
+            // 完全复制目标位置（包括朝向）
+            Location targetLocation = toPlayer.getLocation().clone();
+            fromPlayer.teleport(targetLocation);
+            String insertSql = "INSERT INTO TPA (uuidfrom, uuidto, timestemp, available) VALUES (?, ?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:plugins/BYD_WORLD_UTRAL/database.db");
+             PreparedStatement pstmt = conn.prepareStatement(insertSql)) {
+            // 设置插入的数据
+            pstmt.setString(1, playerfrom);
+            pstmt.setString(2, playerto);
+            pstmt.setLong(3, now_time);
+            pstmt.setInt(4, 1);
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        } else if (type.equalsIgnoreCase("near")) {
+
+        } else {
+
+        }
+        // 播放传送音效
+        fromPlayer.playSound(fromPlayer.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1.0f, 1.0f);
+        toPlayer.playSound(toPlayer.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1.0f, 1.0f);
+
+        return true;
     }
 }
